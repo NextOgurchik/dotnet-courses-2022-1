1a5068481a9826aa7a3add0c2c82150df510ed07
NextOgurchik <mihanfedotov@yandex.ru>
2022-03-09 02:05:47 +0300

    Update User.cs

--- 04-reference-types/ReferenceTypes/Task1/User.cs
+++ 04-reference-types/ReferenceTypes/Task1/User.cs
@@ -4,25 +4,14 @@ namespace Task1
 {
     internal class User
     {
-        private string name;
-        public string Name { get => name; private set { name = string.Empty == value ? value : throw new Exception(); } }
-        private string lastName;
-        public string LastName { get => lastName; private set { lastName = string.Empty == value ? value : throw new Exception(); } }
-        private string patronymic;
-        public string Patronymic { get => patronymic; private set { patronymic = string.Empty == value ? value : throw new Exception(); } }
-        private DateTime dateOfBirth;
-        public DateTime DateOfBirth
-        {
-            get => dateOfBirth;
-            private set
-            {
-                dateOfBirth = DateTime.Now >= value ? value : throw new Exception("User hasn't been born yet.");
-            }
-        }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        private readonly DateTime _dateOfBirth;
 
         public User(DateTime dateOfBirth, string name, string lastName, string patronymic)
         {
-            DateOfBirth = dateOfBirth;
+            _dateOfBirth = DateTime.Now >= dateOfBirth ? dateOfBirth : throw new Exception("User hasn't been born yet.");
             Name = name;
             LastName = lastName;
             Patronymic = patronymic;
@@ -30,14 +19,14 @@ namespace Task1
 
         public int Age()
         {
-            int a = DateTime.Now.Year - DateOfBirth.Year;
-            if (DateTime.Now.Month < DateOfBirth.Month)
+            int a = DateTime.Now.Year - _dateOfBirth.Year;
+            if (DateTime.Now.Month < _dateOfBirth.Month)
             {
                 a--;
             }
-            else if (DateTime.Now.Month == DateOfBirth.Month)
+            else if (DateTime.Now.Month == _dateOfBirth.Month)
             {
-                if (DateTime.Now.Day < DateOfBirth.Day)
+                if (DateTime.Now.Day < _dateOfBirth.Day)
                 {
                     a--;
                 }
@@ -46,7 +35,7 @@ namespace Task1
         }
         public override string ToString()
         {
-            return $"ФИО: {LastName} {Name} {Patronymic}. Дата рождения: {DateOfBirth:D} Возраст: {Age()}";
+            return $"ФИО: {LastName} {Name} {Patronymic}. Дата рождения: {_dateOfBirth:D} Возраст: {Age()}";
         }
     }
 }
