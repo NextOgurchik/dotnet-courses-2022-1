80f87d47cef405f1f704ad457b7d47375d096392
NextOgurchik <mihanfedotov@yandex.ru>
2022-03-09 02:13:12 +0300

    Update User.cs

--- 04-reference-types/ReferenceTypes/Task1/User.cs
+++ 04-reference-types/ReferenceTypes/Task1/User.cs
@@ -7,7 +7,28 @@ namespace Task1
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Patronymic { get; set; }
+
         private readonly DateTime _dateOfBirth;
+        private int age;
+        public int Age
+        {
+            get
+            {
+                age = DateTime.Now.Year - _dateOfBirth.Year;
+                if (DateTime.Now.Month < _dateOfBirth.Month)
+                {
+                    age--;
+                }
+                else if (DateTime.Now.Month == _dateOfBirth.Month)
+                {
+                    if (DateTime.Now.Day < _dateOfBirth.Day)
+                    {
+                        age--;
+                    }
+                }
+                return age;
+            }
+        }
 
         public User(DateTime dateOfBirth, string name, string lastName, string patronymic)
         {
@@ -16,26 +37,10 @@ namespace Task1
             LastName = lastName;
             Patronymic = patronymic;
         }
-
-        public int Age()
-        {
-            int a = DateTime.Now.Year - _dateOfBirth.Year;
-            if (DateTime.Now.Month < _dateOfBirth.Month)
-            {
-                a--;
-            }
-            else if (DateTime.Now.Month == _dateOfBirth.Month)
-            {
-                if (DateTime.Now.Day < _dateOfBirth.Day)
-                {
-                    a--;
-                }
-            }
-            return a;
-        }
+        
         public override string ToString()
         {
-            return $"ФИО: {LastName} {Name} {Patronymic}. Дата рождения: {_dateOfBirth:D} Возраст: {Age()}";
+            return $"ФИО: {LastName} {Name} {Patronymic}. Дата рождения: {_dateOfBirth:D} Возраст: {Age}";
         }
     }
 }
