2009eb656d03646adeb33433bd8bb86bbd595fa1
NextOgurchik <mihanfedotov@yandex.ru>
2022-03-16 17:40:17 +0300

    Fix

--- 05-inheritance/Inheritance/Task2/Program.cs
+++ 05-inheritance/Inheritance/Task2/Program.cs
@@ -11,12 +11,13 @@ namespace Task2
             Console.Write("Введите координату Y: ");
             int.TryParse(Console.ReadLine(), out int y);
             Console.Write("Введите внешний радиус: ");
-            double.TryParse(Console.ReadLine(), out double or);
+            int.TryParse(Console.ReadLine(), out int or);
             Console.Write("Введите внутренний радиус: ");
-            double.TryParse(Console.ReadLine(), out double ir);
+            int.TryParse(Console.ReadLine(), out int ir);
             var p = new Point(x, y);
-            var ring = new Ring(p, or, ir);
+            var ring = new Ring(or, ir, p.X, p.Y);
             ring.Radius = 10;
             Console.WriteLine(ring);
         }
+    }
 }
