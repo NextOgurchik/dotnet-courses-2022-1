cdd5264bf7eb040ff0c25aa924263b13083beafb
NextOgurchik <mihanfedotov@yandex.ru>
2022-03-19 18:28:09 +0300

    Update

---
+++ 07-generics/Generics/Task1/DynamicArray.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class DynamicArray<T>
+    {
+        public int Length { get; private set; }
+        public int Capacity { get { return DArray.Length; } }
+        public T[] DArray { get; set; }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= Length)
+                {
+                    throw new ArgumentOutOfRangeException("Index out of range");
+                }
+                return DArray[index];
+            }
+        }
+        public DynamicArray()
+        {
+            DArray = new T[8];
+        }
+        public DynamicArray(int capacity)
+        {
+            DArray = new T[capacity];
+        }
+        public DynamicArray(T[] array)
+        {
+            DArray = array;
+        }
+        private T[] Copy(T[] array)
+        {
+            var newArray = new T[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            return newArray;
+        }
+        public void Add(T element)
+        {
+            var oldArray = Copy(DArray);
+
+            Length++;
+            if (Length > Capacity)
+            {
+                DArray = new T[Capacity * 2];
+            }
+
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                DArray[i] = oldArray[i];
+            }
+
+            DArray[Length - 1] = element;
+        }
+
+        public void AddRange(T[] array)
+        {
+            var oldArray = Copy(DArray);
+            int capacity = DArray.Length;
+            int startLength = Length;
+            Length += array.Length;
+            if (Length > capacity)
+            {
+                while (Length > capacity)
+                {
+                    capacity *= 2;
+                }
+                DArray = new T[capacity];
+            }
+
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                DArray[i] = oldArray[i];
+            }
+
+            int j = 0;
+            for (int i = startLength; i < Length; i++)
+            {
+                DArray[i] = array[j];
+                j++;
+            }
+        }
+        public bool Remove(int index)
+        {
+            if (index < Length)
+            {
+                Length--;
+                for (int i = index; i < Length; i++)
+                {
+                    DArray[i] = DArray[i + 1];
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Insert(T element, int index)
+        {
+            if (index >= Length)
+            {
+                throw new ArgumentOutOfRangeException("Index out of range");
+            }
+
+            var oldArray = Copy(DArray);
+
+            Length++;
+            if (Length > Capacity)
+            {
+                DArray = new T[Capacity * 2];
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                DArray[i] = oldArray[i];
+            }
+
+            DArray[index] = element;
+
+            for (int i = index; i < Length; i++)
+            {
+                DArray[i + 1] = oldArray[i];
+            }
+        }
+    }
+}
---
+++ 07-generics/Generics/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DArr = new DynamicArray<int>();
+            DArr.Add(10);
+            DArr.Add(21);
+            DArr.Add(32);
+            DArr.Add(43);
+            DArr.Add(54);
+            DArr.Add(65);
+            DArr.Add(76);
+            DArr.Add(87);
+            DArr.Add(98);
+            DArr.Insert(666, 0);
+            DArr.Insert(666, 5);
+            DArr.Insert(666, 5);
+            DArr.Insert(666, 5);
+            DArr.Insert(666, DArr.Length - 1);
+            DArr.Remove(7);
+            DArr.Remove(77);
+            DArr.AddRange(new int[13] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22});
+            Console.WriteLine("Length = " + DArr.Length);
+            Console.WriteLine("Capacity = " + DArr.Capacity);
+            for (int i = 0; i < DArr.Length; i++)
+            {
+                Console.WriteLine(DArr[i]);
+            }
+        }
+    }
+}
